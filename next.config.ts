8c9d3fd1db5e0dd17959ad5bfbc150efb7b5e35b
Thiago Trotta <trottathiago1@gmail.com>
2026-06-07 09:41:54 -0300

    feat: implement global security headers in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,42 @@
 import type { NextConfig } from "next";
 
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: *; font-src 'self' data:; connect-src 'self' *.basemaps.cartocdn.com; worker-src 'self' blob:; frame-ancestors 'none';"
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY"
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff"
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin"
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload"
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on"
+  }
+];
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
